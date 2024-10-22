fa8424d7b8aff1aa119dbf19f7fff50e61bea3b2
M.Taherian <mohammadtaherian77@gmail.com>
2024-10-22 15:37:39 +0330

    Added Turn Animation

--- Assets/Scripts/Scenes/Game/Player/PlayerController.cs
+++ Assets/Scripts/Scenes/Game/Player/PlayerController.cs
@@ -8,18 +8,19 @@ namespace Scenes.Game.Player
     public class PlayerController : MonoBehaviour, ITransformGetter
     {
         [SerializeField] private CharacterController _characterController;
+        [SerializeField] private Animator _animator;
         [SerializeField] private float _moveSpeed;
         [SerializeField] private float _turnSpeed;
+        [SerializeField] private float _animatorTurnSpeed;
         private Vector2 _moveInputValue;
         private Vector2 _aimInputValue;
         private Camera _mainCamera;
+        private float _currentAnimatorTurnSpeed;
+        private static readonly int ForwardSpeedAnimationProperty = Animator.StringToHash("forwardSpeed");
+        private static readonly int RightSpeedAnimationProperty = Animator.StringToHash("rightSpeed");
+        private static readonly int TurnSpeedAnimatorProperty = Animator.StringToHash("turnSpeed");
 
         public event Action<Vector2> PlayerMoved;
-
-        // [Inject]
-        // private void Init()
-        // {
-        // }
 
         private void Start()
         {
@@ -40,10 +41,18 @@ namespace Scenes.Game.Player
         {
             var moveDirection = MovePlayerBasedOnMoveStickInputValue();
             RotatePlayerBasedOnAimDirection(moveDirection);
+
+            // moveDirection * transform.forward = |moveDirection| * |transform.forward| * cos(angle between them)
+            // which draws a line from tip of moveDirection vector perpendicular to transform.forward
+            // and tells us the ratio of moveDirection vector to transform.forward
+            var forwardMovementRatio = Vector3.Dot(moveDirection, transform.forward);
+            var rightMovementRatio = Vector3.Dot(moveDirection, transform.right);
+            _animator.SetFloat(ForwardSpeedAnimationProperty, forwardMovementRatio);
+            _animator.SetFloat(RightSpeedAnimationProperty, rightMovementRatio);
         }
 
         #region Rotation
-        
+
         private void RotatePlayerBasedOnAimDirection(Vector3 moveDirection)
         {
             var aimDirection = moveDirection;
@@ -52,19 +61,30 @@ namespace Scenes.Game.Player
                 aimDirection = JoystickInputToWorldPosition(_aimInputValue);
             }
 
+            float currentFinalTurnSpeed = 0;
             if (aimDirection.sqrMagnitude > 0)
             {
-                RotatePlayer(aimDirection);
+                var previousRotation = transform.rotation;
+                var currentRotation = RotatePlayer(aimDirection);
+
+                var rightAimDirectionRatio = Vector3.Dot(aimDirection, transform.right);
+                var rotationDelta = Quaternion.Angle(previousRotation, currentRotation) * rightAimDirectionRatio;
+                currentFinalTurnSpeed = rotationDelta / Time.deltaTime;
             }
+
+            _currentAnimatorTurnSpeed = Mathf.Lerp(_currentAnimatorTurnSpeed, currentFinalTurnSpeed,
+                Time.deltaTime * _turnSpeed);
+            _animator.SetFloat(TurnSpeedAnimatorProperty, _currentAnimatorTurnSpeed);
         }
-        
-        private void RotatePlayer(Vector3 moveDirection)
+
+        private Quaternion RotatePlayer(Vector3 aimDirection)
         {
             var turnLerpAlpha = _turnSpeed * Time.deltaTime;
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(moveDirection),
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(aimDirection),
                 turnLerpAlpha);
+            return transform.rotation;
         }
-        
+
         #endregion
 
         #region Movement
@@ -80,22 +100,23 @@ namespace Scenes.Game.Player
 
             return moveDirection;
         }
-        
+
         private bool IsMoving()
         {
             return _moveInputValue.sqrMagnitude > 0;
-        } 
+        }
 
         #endregion
 
         private Vector3 JoystickInputToWorldPosition(Vector2 inputValue)
         {
             var rightDirection = _mainCamera.transform.right;
+            // Vector3.Cross returns a vector perpendicular to two given vectors
             var upDirection = Vector3.Cross(rightDirection, Vector3.up);
             var moveDirection = rightDirection * inputValue.x + upDirection * inputValue.y;
             return moveDirection;
         }
-        
+
         public bool IsAiming()
         {
             return _aimInputValue.sqrMagnitude > 0;
